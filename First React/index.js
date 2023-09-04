8a7338d54b193aeea13cd9875f342e0f1fc458a7
Devasya <devasyarajguru@gmail.com>
2023-09-04 20:00:35 +0530

    First React Code

---
+++ First React/index.js
@@ -0,0 +1,5 @@
+// ReactDOM - global variable che
+// render - process of generating or displaying content on Screen(Web Browser)
+// ReactDOM.render(<h1>Hello Devasya</h1> , document.getElementById("root"));
+console.log("Heyy")
+
